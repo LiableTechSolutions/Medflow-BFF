0d5b231ffe03ef74136173ce1a858622fae95005
techmohtox-create <techmohtox@gmail.com>
2026-07-25 20:10:39 +0530

    Stable code

--- target/generated-sources/annotations/com/medflow/modules/notifications/mapper/NotificationMapperImpl.java
+++ target/generated-sources/annotations/com/medflow/modules/notifications/mapper/NotificationMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-25T19:48:22+0530",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
+    date = "2026-07-25T20:09:14+0530",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.7 (Oracle Corporation)"
 )
 @Component
 public class NotificationMapperImpl implements NotificationMapper {
